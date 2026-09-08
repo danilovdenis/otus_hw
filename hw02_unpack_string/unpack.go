125f2b1bc2332587313cede602ddc5bae1ae5450
Danilov Denis <danilov.ds@dns-shop.ru>
2026-09-09 09:54:24 +1000

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const backslashCode = 92
+
+func Unpack(str string) (string, error) {
+	var builder strings.Builder
+
+	runes := []rune(str)
+
+	for i := 0; i < len(runes); {
+		var ch rune
+
+		if backslashCode == runes[i] {
+			if i+1 >= len(runes) {
+				return "", ErrInvalidString
+			}
+
+			next := runes[i+1]
+
+			if !unicode.IsDigit(next) && backslashCode != next {
+				return "", ErrInvalidString
+			}
+
+			ch = next
+			i += 2
+		} else if unicode.IsDigit(runes[i]) {
+			return "", ErrInvalidString
+		} else {
+			ch = runes[i]
+			i++
+		}
+
+		cnt := 1
+		if i < len(runes) && unicode.IsDigit(runes[i]) {
+			cnt, _ = strconv.Atoi(string(runes[i]))
+			i++
+		}
+
+		builder.WriteString(strings.Repeat(string(ch), cnt))
+	}
+
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,11 +18,25 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "🙃0", expected: ""},
+		{input: "aaф0b", expected: "aab"},
+		{input: "😀3", expected: "😀😀😀"},
+		{input: "日本2語", expected: "日本本語"},
+		{input: "ä2ö3", expected: "ääööö"},
+		{input: "я2а3", expected: "яяааа"},
+		{input: "キ2ャ3", expected: "キキャャャ"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "a\t2b", expected: "a\t\tb"},
+		{input: "a\r2b", expected: "a\r\rb"},
+		{input: "ф2я3", expected: "ффяяя"},
+		{input: "🙂2😀3", expected: "🙂🙂😀😀😀"},
+		{input: "тест2!", expected: "тестт!"},
+		// доп задание
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -36,7 +50,16 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{
+		"3abc",
+		"45",
+		"aaa10b",
+		"3привет",
+		`qw\e`,
+		`qw\n`,
+		`qw\`,
+		`qw\\\`,
+	}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
