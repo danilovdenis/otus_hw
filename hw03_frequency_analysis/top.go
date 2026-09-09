4f6c6612fac1856a752ec5442532299b1edf3b82
Danilov Denis <danilov.ds@dns-shop.ru>
2026-09-09 18:56:38 +1000

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,67 @@
-package hw03frequencyanalysis
+package main
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"maps"
+	"slices"
+	"sort"
+	"strings"
+)
+
+// Простой вариант
+//func Top10(str string) []string {
+//	words := make(map[string]int)
+//
+//	fields := strings.Fields(str)
+//
+//	for _, word := range fields {
+//		words[word] += 1
+//	}
+//
+//	keys := slices.Collect(maps.Keys(words))
+//
+//	sort.Slice(keys, func(i, j int) bool {
+//		if words[keys[i]] == words[keys[j]] {
+//			return keys[i] < keys[j]
+//		}
+//		return words[keys[i]] > words[keys[j]]
+//	})
+//
+//	if len(keys) > 10 {
+//		keys = keys[:10]
+//	}
+//
+//	return keys
+//}
+
+// Вариант со звездочкой
+func Top10(str string) []string {
+	words := make(map[string]int)
+
+	fields := strings.Fields(str)
+
+	for _, word := range fields {
+		clearWord := strings.ToLower(word)
+		clearWord = strings.Trim(clearWord, ".,:;()[]")
+
+		if clearWord == "-" {
+			continue
+		}
+
+		words[strings.ToLower(clearWord)] += 1
+	}
+
+	keys := slices.Collect(maps.Keys(words))
+
+	sort.Slice(keys, func(i, j int) bool {
+		if words[keys[i]] == words[keys[j]] {
+			return keys[i] < keys[j]
+		}
+		return words[keys[i]] > words[keys[j]]
+	})
+
+	if len(keys) > 10 {
+		keys = keys[:10]
+	}
+
+	return keys
 }
