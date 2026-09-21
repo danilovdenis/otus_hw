a5f5ea823ab34a402af02bf33ecf7e99e4f97c96
Denis Danilov <danilov.ds1@dns-shop.ru>
2026-09-21 15:17:11 +1000

    HW3 is completed | рефакторинг функции

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -9,22 +9,19 @@ import (
 )
 
 func Top10(str string, advanced bool) []string {
-	// Для чистоты кода можно сделать одну функию т.к. это дублирование кода.
-	// Вынести в отдельную функцию метод очистки слов и применять этот опараметр там.
-	if advanced {
-		return top10Advanced(str)
-	}
-
-	return top10Simple(str)
-}
-
-// Простой вариант.
-func top10Simple(str string) []string {
 	words := make(map[string]int)
 
 	fields := strings.Fields(str)
 
 	for _, word := range fields {
+		if advanced {
+			word = clearWord(word)
+
+			if word == "-" {
+				continue
+			}
+		}
+
 		words[word]++
 	}
 
@@ -44,37 +41,11 @@ func top10Simple(str string) []string {
 	return keys
 }
 
-// Вариант со звездочкой.
-func top10Advanced(str string) []string {
-	words := make(map[string]int)
-
-	fields := strings.Fields(str)
-
-	for _, word := range fields {
-		clearWord := strings.ToLower(word)
-		clearWord = strings.TrimFunc(clearWord, func(r rune) bool {
-			return !unicode.IsLetter(r) && !unicode.IsNumber(r) && r != '-'
-		})
-
-		if clearWord == "-" {
-			continue
-		}
-
-		words[clearWord]++
-	}
-
-	keys := slices.Collect(maps.Keys(words))
-
-	sort.Slice(keys, func(i, j int) bool {
-		if words[keys[i]] == words[keys[j]] {
-			return keys[i] < keys[j]
-		}
-		return words[keys[i]] > words[keys[j]]
+func clearWord(word string) string {
+	word = strings.ToLower(word)
+	word = strings.TrimFunc(word, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r) && r != '-'
 	})
 
-	if len(keys) > 10 {
-		keys = keys[:10]
-	}
-
-	return keys
+	return word
 }
